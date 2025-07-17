12eee597070373eaefb673d7c95a89550ebde7bd
wangziyi <2389794787@qq.com>
2025-07-17 09:49:31 +0800

    在uninitialized.hpp文件中添加了uninitialized_fill、uninitialized_move和相关的unchecked版本实现，增强了内存管理功能，支持不同类型的元素填充和移动。

--- src/uninitialized.hpp
+++ src/uninitialized.hpp
@@ -91,6 +91,42 @@ uninitialized_copy_n(InputIter first, Size n, ForwardIter result)
 }
 
 /*****************************************************************************************/
+// uninitialized_fill
+// 在 [first, last) 区间内填充元素值
+/*****************************************************************************************/
+template<class ForwardIter, class T>
+void unchecked_uninit_fill(ForwardIter first, ForwardIter last, const T& value, std::true_type)
+{
+    wzy_stl::fill(first, last, value);
+}
+
+template<class ForwardIter, class T>
+void unchecked_uninit_fill(ForwardIter first, ForwardIter last, const T& value, std::false_type)
+{
+    auto cur = first;
+    try{
+        for(; cur != last; ++cur)
+        {
+            wzy_stl::construct(&*cur, value);
+        }
+    }
+    catch(...)
+    {
+        for(; first != cur; ++first)
+        {
+            wzy_stl::destroy(&*first);
+        }
+    }
+}
+
+template<class ForwardIter, class T>
+void uninitialized_fill(ForwardIter first, ForwardIter last, const T& value)
+{
+    wzy_stl::unchecked_uninit_fill(first, last, value,
+    std::is_trivially_copy_constructible<typename iterator_traits<ForwardIter>::value_type>{});
+}
+
+/*****************************************************************************************/
 // uninitialized_fill_n
 // 从 first 位置开始，填充 n 个元素值，返回填充结束的位置
 /*****************************************************************************************/
@@ -128,6 +164,87 @@ uninitialized_fill_n(ForwardIter first, Size n, const T& value)
 {
     return wzy_stl::unchecked_uninit_fill_n(first, n, value,
     std::is_trivially_copy_assignable<typename iterator_traits<ForwardIter>::value_type>{});
+}
+
+/*****************************************************************************************/
+// uninitialized_move
+// 把[first, last)上的内容移动到以 result 为起始处的空间，返回移动结束的位置
+/*****************************************************************************************/
+template<class InputIter, class ForwardIter>
+ForwardIter
+unchecked_uninit_move(InputIter first, InputIter last, ForwardIter result, std::true_type)
+{
+    return wzy_stl::move(first, last, result);
+}
+
+template<class InputIter, class ForwardIter>
+ForwardIter
+unchecked_uninit_move(InputIter first, InputIter last, ForwardIter result, std::false_type)
+{
+    ForwardIter cur = result;
+    try{
+        for(; first != last; ++cur, ++first)
+        {
+            wzy_stl::construct(&*cur, wzy_stl::move(*first));
+        }
+    }
+    catch(...)
+    {
+        wzy_stl::destroy(result, cur);
+    }
+    return cur;
+}
+
+template <class InputIter, class ForwardIter>
+ForwardIter uninitialized_move(InputIter first, InputIter last, ForwardIter result)
+{
+  return wzy_stl::unchecked_uninit_move(first, last, result,
+                                      std::is_trivially_move_assignable<
+                                      typename iterator_traits<InputIter>::
+                                      value_type>{});
+}
+
+/*****************************************************************************************/
+// uninitialized_move_n
+// 把[first, first + n)上的内容移动到以 result 为起始处的空间，返回移动结束的位置
+/*****************************************************************************************/
+
+template<class InputIter, class Size, class ForwardIter>
+ForwardIter
+unchecked_uninit_move_n(InputIter first, Size n, ForwardIter result, std::true_type)
+{
+    return wzy_stl::move_n(first, n, result);
+}
+
+template<class InputIter, class Size, class ForwardIter>
+ForwardIter
+unchecked_uninit_move_n(InputIter first, Size n, ForwardIter result, std::false_type)
+{
+    auto cur = result;
+    try
+    {
+        for(; n > 0; --n, ++cur, ++first)
+        {
+            wzy_stl::construct(&*cur, wzy_stl::move(*first));
+        }
+    }
+    catch(...)
+    {
+        for(; result != cur; ++result)
+        {
+            wzy_stl::destroy(&*result);
+        }
+    }
+    return cur;
+}
+
+template <class InputIter, class Size, class ForwardIter>
+ForwardIter uninitialized_move_n(InputIter first, Size n, ForwardIter result)
+{
+  return wzy_stl::unchecked_uninit_move_n(first, n, result,
+                                        std::is_trivially_move_assignable<
+                                        typename iterator_traits<InputIter>::
+                                        value_type>{});
 }
 
 }  // namespace wzy_stl
